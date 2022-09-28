eeffcc5ec022a0ed11dc26731b3e26caba8d33a7
Harold Pinilla <hdpinilla@javeriana.edu.co>
2022-09-27 19:13:53 -0500

    Vinculando articulos en la tienda

--- tienda.py
+++ tienda.py
@@ -1,4 +1,4 @@
-from articulo import articulos as items
+from articulo import Articulo
 
 class Tienda:
 
@@ -16,11 +16,16 @@ class Tienda:
         print("-------------------------------------------------------------------------\n")
 
 articulos = list()
-articulos.append(items[0])
-articulos.append(items[1])
-articulos.append(items[3])
-articulos.append(items[4])
-articulos.append(items[7])
+articulos.append(Articulo(1, "Televisor", 2200000))
+articulos.append(Articulo(2, "Lavadora", 1200000))
+articulos.append(Articulo(3, "Nevera", 2430000))
+articulos.append(Articulo(4, "Computadora", 1800000))
+articulos.append(Articulo(5, "Tostadora", 315000))
+articulos.append(Articulo(6, "Equipo de sonido", 895000))
+articulos.append(Articulo(7, "Estufa", 745000))
+articulos.append(Articulo(8, "Silla", 130000))
+articulos.append(Articulo(9, "Celular", 900000))
+articulos.append(Articulo(10, "Moto", 7610000))
 
 tienda = Tienda(123456, "Electronics SA", articulos)    
 # tienda.imprimir()
